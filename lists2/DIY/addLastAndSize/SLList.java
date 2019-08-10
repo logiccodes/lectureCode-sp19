a40a78b8bce580a77f26a4774c168c3e617afc66
huixutech <19688861+logiccodes@users.noreply.github.com>
2019-08-11 07:10:48 +0800

    chapter 2.3 excercises

--- lists2/DIY/addLastAndSize/SLList.java
+++ lists2/DIY/addLastAndSize/SLList.java
@@ -27,11 +27,41 @@ public class SLList {
     /** Adds an item to the end of the list. */
     public void addLast(int x) {
         /* Your Code Here! */
+        IntNode node = first;
+        while (node.next != null) {
+            node = node.next;
+        }
+        node.next = new IntNode(x, null);
     }
 
+    /** we'll create a private helper method that 
+    interacts with the underlying naked recursive data structure.*/
+    private static int size(IntNode p) {
+        if (p.next == null) {
+            return 1;
+        }
+        return 1 + size(p.next);
+    }
     /** Returns the number of items in the list using recursion. */
     public int size() {
         /* Your Code Here! */
+        /*a recursive implementation of size, 
+         which will yield an interesting challenge.
+         IntNode class don't have size().*/
+        /* first is lost.*/
+        /*
+        if (first.next == null){
+            return 1;
+        } 
+        first = first.next;
+        return 1 + size();*/
+        return size(first);
     }
 
+    public static void main(String[] args) {
+        SLList L = new SLList(10);
+        L.addLast(20);
+        System.out.println(L.size());
+        System.out.println(L.getFirst());
+    }
 }
--- lists2/DIY/fixAddLast/SLList.java
+++ lists2/DIY/fixAddLast/SLList.java
@@ -8,28 +8,33 @@ public class SLList {
         }
     }
 
-    private IntNode first; 
+    //private IntNode first; 
+    private IntNode sentinel;
     private int size;
 
     public SLList() {
-        first = null;
+        //first = null;
+        sentinel = new IntNode(-35672, null);
         size = 0;
     }
 
     public SLList(int x) {
-        first = new IntNode(x, null);
+        //first = new IntNode(x, null);
+        sentinel.next = new IntNode(x, null);
         size = 1;
     }
 
     /** Adds an item to the front of the list. */
     public void addFirst(int x) {
-        first = new IntNode(x, first);
+        //first = new IntNode(x, first);
+        sentinel.next = new IntNode(x, sentinel.next);
         size += 1;
     }    
 
     /** Retrieves the front item from the list. */
     public int getFirst() {
-        return first.item;
+        //return first.item;
+        return sentinel.next.item;
     }
 
     /** Returns the number of items in the list. */
@@ -39,18 +44,34 @@ public class SLList {
 
     /** Adds an item to the end of the list. */
     public void addLast(int x) {
-        IntNode p = first;
-
+        //IntNode p = first;
+        IntNode p = sentinel;
+        /**
+        Human beings only have so much working memory, 
+        and thus we want to keep complexity under control 
+        wherever possible. For a simple data structure 
+        like the SLList, the number of special cases is small. 
+        More complicated data structures like trees can get much, much uglier.
+        */
+        /*
+        if (p == null) {
+            p = new IntNode(x, null);
+            return;
+        }*/
         /* Advance p to the end of the list. */
         while (p.next != null) {
             p = p.next;
         }
         p.next = new IntNode(x, null);
+        size += 1;
     }
 
     /** Crashes when you call addLast on an empty SLList. Fix it. */
     public static void main(String[] args) {
         SLList x = new SLList();
         x.addLast(5);
+        x.addLast(10);
+        System.out.println(x.size());
+        System.out.println(x.getFirst());
     }
 }
